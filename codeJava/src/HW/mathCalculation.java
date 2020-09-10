25ee744938776ca8a1366bb41f24e0972099038e
Hieu Truong <thanhhieux9@gmail.com>
2020-09-10 20:47:32 +0700

    fix_code

--- codeJava/src/HW/mathCalculation.java
+++ codeJava/src/HW/mathCalculation.java
@@ -4,107 +4,102 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
-public class mathCalculation extends JFrame implements ActionListener {
+public class mathCalculation extends JFrame {
 
-	private JButton bt1, bt2, bt3, bt4;
-
-	private JTextField tf1, tf2, tf3;
-
-	private double result;
-
-	private Container cont = getContentPane();
-
-	private JPanel panel1, panel2;
-
-	public mathCalculation(String s) {
-		super(s);
-		cont.setPreferredSize(new Dimension(500, 200));
-		JLabel num1lb = new JLabel("first: ");
-		tf1 = new JTextField();
-		JLabel num2lb = new JLabel("Seccond: ");
-		tf2 = new JTextField();
-		JLabel resultlb = new JLabel("Result: ");
-		tf3 = new JTextField();
-		tf3.setEditable(false);
-
-		panel1 = new JPanel();
-		GridLayout grlayout = new GridLayout(3, 2, 20, 15);
-		panel1.setLayout(grlayout);
-		panel1.add(num1lb);
-		panel1.add(tf1);
-		panel1.add(num2lb);
-		panel1.add(tf2);
-		panel1.add(resultlb);
-		panel1.add(tf3);
-
-		bt1 = new JButton("+");
-		bt2 = new JButton("-");
-		bt3 = new JButton("*");
-		bt4 = new JButton("/");
-		panel2 = new JPanel();
-		panel2.setLayout(new FlowLayout(1, 50, 20));
-
-		bt1.addActionListener(this);
-		bt2.addActionListener(this);
-		bt3.addActionListener(this);
-		bt4.addActionListener(this);
-
-		panel2.add(bt1);
-		panel2.add(bt2);
-		panel2.add(bt3);
-		panel2.add(bt4);
-
-		cont.add(panel1);
-		cont.add(panel2, "South");
-		this.pack();
-		this.setVisible(true);
-	}
-
-	public void doPlus() {
-				
-			bt1.setEnabled(true);
-			result = Double.parseDouble(tf1.getText()) + Double.parseDouble(tf2.getText());
-			tf3.setText(String.valueOf(result));
-		
-		
-	}
-
-	public void doMinus() {
-		result = Double.parseDouble(tf1.getText()) - Double.parseDouble(tf2.getText());
-		tf3.setText(String.valueOf(result));
-	}
-
-	public void doMult() {
-		result = Double.parseDouble(tf1.getText()) * Double.parseDouble(tf2.getText());
-		tf3.setText(String.valueOf(result));
-	}
-
-	public void doDiv() {
-		result = Double.parseDouble(tf1.getText()) / Double.parseDouble(tf2.getText());
-		tf3.setText(String.valueOf(result));
-	}
-
-	public void name(JButton bt12) {
-		if (tf1.getText() == null || tf2.getText() == null) {
-			bt12.setEnabled(true);
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand() == "+")
-			doPlus();
-		if (e.getActionCommand() == "-")
-			doMinus();
-		if (e.getActionCommand() == "*")
-			doMult();
-		if (e.getActionCommand() == "/")
-			doDiv();
-
-	}
+	private JButton Bt1, Bt2, Bt3, Bt4;
+	private JTextField Tf1, Tf2, Tf3;
+	private double Result;
+	private Container Conn = getContentPane();
+	private JPanel Pn1, Pn2;
 
 	public static void main(String args[]) {
 		mathCalculation mathCalculation = new mathCalculation("mathCalculation");
 		mathCalculation.setLocationRelativeTo(null);
 	}
+	public mathCalculation(String tt) {
+		super(tt);
+		Conn.setPreferredSize(new Dimension(500, 200));
+		JLabel Lb1 = new JLabel("first: ");
+		Tf1 = new JTextField();
+		JLabel Lb2 = new JLabel("Seccond: ");
+		Tf2 = new JTextField();
+		JLabel Lb3 = new JLabel("Result: ");
+		Tf3 = new JTextField();
+		Tf3.setEditable(false);
+
+		Pn1 = new JPanel();
+		GridLayout gr = new GridLayout(3, 2, 20, 15);
+		Pn1.setLayout(gr);
+		Pn1.add(Lb1);
+		Pn1.add(Tf1);
+		Pn1.add(Lb2);
+		Pn1.add(Tf2);
+		Pn1.add(Lb3);
+		Pn1.add(Tf3);
+
+		Bt1 = new JButton("+");
+		Bt2 = new JButton("-");
+		Bt3 = new JButton("*");
+		Bt4 = new JButton("/");
+		Pn2 = new JPanel();
+		Pn2.setLayout(new FlowLayout(1, 50, 20));
+
+		Pn2.add(Bt1);
+		Pn2.add(Bt2);
+		Pn2.add(Bt3);
+		Pn2.add(Bt4);
+
+		Conn.add(Pn1);
+		Conn.add(Pn2, "South");
+		
+		funtionbutton();
+		this.pack();
+		this.setVisible(true);
+	}
+	private void funtionbutton() {
+		BtPlusEvent();
+		BtMinusEvent();
+		BtMultEvent();
+		BtDivEvent();
+	}
+	private void BtPlusEvent() {
+		Bt1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Result = Double.parseDouble(Tf1.getText()) + Double.parseDouble(Tf2.getText());
+				Tf3.setText(String.valueOf(Result));
+			}
+		});
+
+	}
+	private void BtMinusEvent() {
+		Bt2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Result = Double.parseDouble(Tf1.getText()) - Double.parseDouble(Tf2.getText());
+				Tf3.setText(String.valueOf(Result));
+			}
+		});
+
+	}
+	private void BtMultEvent() {
+		Bt3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Result = Double.parseDouble(Tf1.getText()) * Double.parseDouble(Tf2.getText());
+				Tf3.setText(String.valueOf(Result));
+			}
+		});
+
+	}
+	private void BtDivEvent() {
+		Bt4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Result = Double.parseDouble(Tf1.getText()) / Double.parseDouble(Tf2.getText());
+				Tf3.setText(String.valueOf(Result));
+			}
+		});
+	}
+	
 }
